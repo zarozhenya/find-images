3eced09b7b39e5c20861e0e4794d2f9836a412be
Yevhen <eug.z2004@gmail.com>
2022-12-12 13:14:36 +0200

    reorganized repository structure

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>Find image</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
