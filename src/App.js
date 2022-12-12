f524b5c38d5ec1cac66472265f62ffd05390e9d5
Yevhen <eug.z2004@gmail.com>
2022-12-12 14:16:05 +0200

    integrated redux

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import {Provider} from 'react-redux';
 import {Navigation} from './navigation';
+import {store} from './redux/store';
 const App = () => {
   return (
-    <>
+    <Provider store={store}>
       <Navigation />
-    </>
+    </Provider>
   );
 };
 
--- src/navigation/screens/home/index.js
+++ src/navigation/screens/home/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import {SafeAreaView, Text} from 'react-native';
+import {useDispatch, useSelector} from 'react-redux';
+import {selectPage, selectQuery} from '../../../redux/imagesSlice';
+import {increment} from '../../../redux/imagesSlice';
 
 export const HomeScreen = () => {
+  const dispatch = useDispatch();
+  const page = useSelector(selectPage);
+  const query = useSelector(selectQuery);
   return (
     <SafeAreaView>
-      <Text>Home page</Text>
+      <Text
+        onPress={() => {
+          dispatch(increment());
+        }}>
+        Page: {page}
+      </Text>
+      <Text>Query: {query}</Text>
     </SafeAreaView>
   );
 };
