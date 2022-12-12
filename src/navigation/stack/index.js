45e5896c878c951f8ccd5496bff2e72be1463b34
Yevhen <eug.z2004@gmail.com>
2022-12-12 14:45:48 +0200

    added input

--- src/navigation/stack/index.js
+++ src/navigation/stack/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {HomeScreen} from '../screens/home';
+import {HomeScreen} from '../screens/HomeScreen';
 const Stack = createNativeStackNavigator();
 
 export const MainStack = () => {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{headerShown: false}}
+      />
     </Stack.Navigator>
   );
 };
